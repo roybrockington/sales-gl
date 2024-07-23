37f6932c9c142114e2300b4d4f2a30309b819d99
roybrockington <r.brockington@gmail.com>
2024-07-23 16:37:31 +0100

    2d blobs showing correctly

--- src/components/Scatter.tsx
+++ src/components/Scatter.tsx
@@ -1,15 +1,12 @@
 import { ScatterplotLayer } from "@deck.gl/layers";
-import salesData from '../assets/sales.example.json'
+import salesData from '../assets/sales.json'
 
 const Scatter = () => new ScatterplotLayer({
     id: 'scatter',
     data: salesData,
-    opacity: 0.8,
-    filled: true,
-    radiusMinPixels: 5,
-    radiusMaxPixels: 8,
-    getPosition: d => [d.latitude, d.longitude],
+    getPosition: d => [d.longitude, d.latitude],
     getFillColor: d => d.value > 100000 ? [200, 0, 40, 150] : [255, 140, 0, 100],
+    getRadius: 8000,
 })
 
 export default Scatter
